843dc67aea40f9d3fbc5962052c6b5dedd291b2b
Ivan Magda <imagda15@gmail.com>
2017-03-14 19:04:48 +0900

    chore: Build VideoList

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyD8oM4bUFHt9Mazt4chUP1__SSju1Vt9x8';
 
-// Create a new component.
-// This component should produce some HTML.
+class App extends Component {
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      // this.setState({ videos: videos })
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
-// Take this component's generated HTML and put it on the page (DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
