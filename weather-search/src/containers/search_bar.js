ae067224e86c44aa415e7977d2f323db6ce9c00d
Ivan Magda <imagda15@gmail.com>
2017-03-15 12:22:14 +0900

    chore: Control input change in the SearchBar and bind context

--- weather-search/src/containers/search_bar.js
+++ weather-search/src/containers/search_bar.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({ term: event.target.vlaue})
+  }
+
   render () {
     return (
       <form className="input-group">
-        <input />
+        <input
+          placeholder="Get a five-day forecast in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange} />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
       </form>
     );
   }
+
 }
 
 export default SearchBar;
