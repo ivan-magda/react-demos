0e5be3866c6efdd5730a2a7a1f18ba758299a49b
Ivan Magda <imagda15@gmail.com>
2017-03-16 11:14:08 +0900

    chore: Create posts index route

--- redux-blog/src/routes.js
+++ redux-blog/src/routes.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
+import PostsIndex from './components/posts_index';
 
 export default (
-  <Route path="/" component={App} />
+  <Route path="/" component={App}>
+    <IndexRoute component={PostsIndex} />
+  </Route>
 );
