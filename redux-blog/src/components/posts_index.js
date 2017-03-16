4bed865e8b3ad3cf77ef8c7531991fd5606b40b5
Ivan Magda <imagda15@gmail.com>
2017-03-16 11:57:49 +0900

    chore: Navigate to the create new post screen

--- redux-blog/src/components/posts_index.js
+++ redux-blog/src/components/posts_index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
 import { fetchPosts } from '../actions/index';
 
@@ -11,7 +12,14 @@ class PostsIndex extends Component {
 
   render() {
     return (
-      <div>List of blog posts</div>
+      <div>
+        <div className="text-xs-right">
+          <Link to="/posts/new" className="btn btn-primary">
+            Add a Post
+          </Link>
+        </div>
+        List of blog posts
+      </div>
     );
   }
 
