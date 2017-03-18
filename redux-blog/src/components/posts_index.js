b146024cc52c2b14f4957e0bb3fea45dff8e224b
Ivan Magda <imagda15@gmail.com>
2017-03-18 13:51:10 +0900

    chore: Dynamic navigation to PostsShow component

--- redux-blog/src/components/posts_index.js
+++ redux-blog/src/components/posts_index.js
@@ -10,6 +10,19 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    return this.props.posts.map((post) => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          <Link to={`posts/${post.id}`}>
+            <strong>{post.title}</strong>
+          </Link>
+          <span className="pull-xs-right">{post.categories}</span>
+        </li>
+      );
+    });
+  }
+
   render() {
     return (
       <div>
@@ -18,11 +31,18 @@ class PostsIndex extends Component {
             Add a Post
           </Link>
         </div>
-        List of blog posts
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
 
 }
 
-export default connect(null, { fetchPosts })(PostsIndex);
+function mapStateToProps(state) {
+  return { posts: state.posts.all }
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
