2d91558f753cdf68c1ee21bcbc207d9ebe700705
Ivan Magda <imagda15@gmail.com>
2017-03-18 14:22:35 +0900

    chore: Delete blog post

--- redux-blog/src/actions/index.js
+++ redux-blog/src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
 export const FETCH_POST = 'FETCH_POST';
+export const DELETE_POST = 'DELETE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=4f31740b-18dc-4ad0-80db-85adcb5bb94i';
@@ -28,6 +29,14 @@ export function fetchPost(id) {
   const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
   return {
     type: FETCH_POST,
+    payload: request
+  };
+}
+
+export function deletePost(id) {
+  const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`);
+  return {
+    type: DELETE_POST,
     payload: request
   };
 }
--- redux-blog/src/components/posts_show.js
+++ redux-blog/src/components/posts_show.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions/index';
+import { Link } from 'react-router';
+import { fetchPost, deletePost } from '../actions/index';
 
 class PostsShow extends Component {
 
   componentWillMount() {
     this.props.fetchPost(this.props.params.id);
+  }
+
+  onDeleteClick() {
+    this.props.deletePost(this.props.params.id);
   }
 
   render() {
@@ -17,6 +22,13 @@ class PostsShow extends Component {
 
     return (
       <div>
+        <Link to="/">Back</Link>
+        <button
+          className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick.bind(this)}>
+          Delete Post
+        </button>
+
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
@@ -30,4 +42,4 @@ function mapStateToProps(state) {
   return { post: state.posts.post };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
