53b6217266dae9ea964a7a45f94b2be286e6525f
Ivan Magda <imagda15@gmail.com>
2017-03-18 13:12:20 +0900

    chore: Navigate on submit new post

--- redux-blog/src/components/posts_new.js
+++ redux-blog/src/components/posts_new.js
@@ -1,15 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
 import { Link } from 'react-router';
 
 class PostsNew extends Component {
+
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  onSubmit(props) {
+    this.props.createPost(props)
+      .then(() => {
+        // blog post has been created, navigate the user to the index
+        // We navigate by calling this.context.router.push with the
+        // new path to navigate to.
+        this.context.router.push('/');
+      });
+  }
+
   render() {
     const { fields: { title, categories, content }, handleSubmit } = this.props;
 
     return (
-      <form onSubmit={handleSubmit(this.props.createPost)}>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <h3>Create a New Post</h3>
+
         <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
           <label>Title</label>
           <input type="text" className="form-control" {...title} />
@@ -17,6 +33,7 @@ class PostsNew extends Component {
             { title.touched ? title.error : '' }
           </div>
         </div>
+
         <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
           <label>Categories</label>
           <input type="text" className="form-control" {...categories} />
@@ -24,6 +41,7 @@ class PostsNew extends Component {
             { categories.touched ? categories.error : '' }
           </div>
         </div>
+
         <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
           <label>Content</label>
           <textarea type="text" className="form-control" {...content} />
@@ -31,6 +49,7 @@ class PostsNew extends Component {
             { content.touched ? content.error : '' }
           </div>
         </div>
+
         <button type="submit" className="btn btn-primary">Submit</button>
         <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
