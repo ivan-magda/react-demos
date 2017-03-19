99309ebd1b48b2fde31132ec7faa9fb4365a5f19
Ivan Magda <imagda15@gmail.com>
2017-03-20 02:40:44 +0900

    [NATIVE] Albums - Add a 'Buy Now' button

--- react-native/Albums/src/components/AlbumDetail.js
+++ react-native/Albums/src/components/AlbumDetail.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image, image } = album;
+  const { title, artist, thumbnail_image, image, url } = album;
   const {
     headerContent,
     thumbnailImage,
@@ -28,11 +29,18 @@ const AlbumDetail = ({ album }) => {
           <Text>{artist}</Text>
         </View>
       </CardSection>
+
       <CardSection>
         <Image
           source={{ uri: image }}
           style={imageStyle}
         />
+      </CardSection>
+
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url)}>
+          Buy Now
+        </Button>
       </CardSection>
     </Card>
   );
