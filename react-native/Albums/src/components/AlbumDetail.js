5259ea025dd17db3d5833df75c45e9d2f1e57a7c
Ivan Magda <imagda15@gmail.com>
2017-03-20 02:06:16 +0900

    [NATIVE] Albums - Show album artwork

--- react-native/Albums/src/components/AlbumDetail.js
+++ react-native/Albums/src/components/AlbumDetail.js
@@ -5,22 +5,34 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image } = album;
-  const { headerContent, thumbnailImage } = styles;
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    headerContent,
+    thumbnailImage,
+    thumbnailImageContainer,
+    headerText,
+    imageStyle
+  } = styles;
 
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailImageContainer}>
           <Image
             style={thumbnailImage}
             source={{ uri: thumbnail_image }}
           />
         </View>
         <View style={headerContent}>
-          <Text>{title}</Text>
+          <Text style={headerText}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          source={{ uri: image }}
+          style={imageStyle}
+        />
       </CardSection>
     </Card>
   );
@@ -31,9 +43,23 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around'
   },
+  headerText: {
+    fontSize: 18
+  },
   thumbnailImage: {
     width: 50,
     height: 50
+  },
+  thumbnailImageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 8,
+    marginRight: 8
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 
