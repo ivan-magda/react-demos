caa825dedf07c0f37f272c76aa80848d71da41a0
Ivan Magda <imagda15@gmail.com>
2017-03-19 23:30:08 +0900

    [NATIVE] Albums - Create a Header component

---
+++ react-native/Albums/src/components/header.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet
+} from 'react-native';
+
+const Header = ({ title }) => {
+  const { textStyle, container } = styles;
+  return (
+    <View style={container}>
+      <Text style={textStyle}>
+        {title}
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#f8f8f8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 64,
+    paddingTop: 8,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0.5 },
+    shadowOpacity: 0.15,
+    elevation: 2,
+    position: 'relative'
+  },
+  textStyle: {
+    fontSize: 20
+  }
+});
+
+export default Header;
