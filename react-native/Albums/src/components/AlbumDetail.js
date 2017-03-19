0163788939cb339a14366317a130dff0a6a1a454
Ivan Magda <imagda15@gmail.com>
2017-03-20 01:55:21 +0900

    [NATIVE] Albums - Show album thumbnail image

--- react-native/Albums/src/components/AlbumDetail.js
+++ react-native/Albums/src/components/AlbumDetail.js
@@ -1,18 +1,40 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 
 import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-  const { title } = album;
+  const { title, artist, thumbnail_image } = album;
+  const { headerContent, thumbnailImage } = styles;
+
   return (
     <Card>
       <CardSection>
-        <Text>{title}</Text>
+        <View>
+          <Image
+            style={thumbnailImage}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={headerContent}>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
       </CardSection>
     </Card>
   );
+};
+
+const styles = {
+  headerContent: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  thumbnailImage: {
+    width: 50,
+    height: 50
+  }
 };
 
 export default AlbumDetail;
