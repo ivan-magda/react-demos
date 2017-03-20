a5836447d49733fb82d89d0c00f5dce380c76316
Ivan Magda <imagda15@gmail.com>
2017-03-21 01:15:20 +0900

    [Native] Auth - Firebase setup

--- react-native/Auth/src/App.js
+++ react-native/Auth/src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 
 import { Header } from './components/common';
 
 class App extends Component {
-  render() {
-    return (
-      <View>
-        <Header title="Authentication"/>
-        <Text>An App!</Text>
-      </View>
-    );
-  }
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyB45Oql3bN5EIDnRzt_krWMcjQWB-SgQY4",
+            authDomain: "auth-b2fce.firebaseapp.com",
+            databaseURL: "https://auth-b2fce.firebaseio.com",
+            storageBucket: "auth-b2fce.appspot.com",
+            messagingSenderId: "716119981017"
+        });
+    }
+
+    render() {
+        return (
+            <View>
+                <Header title="Authentication"/>
+                <Text>An App!</Text>
+            </View>
+        );
+    }
 }
 
 export default App;
