b83a5740a7e9539a1793f1e6c0886a9538b408a0
Ivan Magda <imagda15@gmail.com>
2017-03-22 12:56:14 +0900

    [Native] TechStack - Create store

--- react-native/TechStack/src/App.js
+++ react-native/TechStack/src/App.js
@@ -1,12 +1,19 @@
+
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import reducers from './reducers';
 
 export default class App extends Component {
     render() {
         return (
-            <View>
-                <Text>Hello World</Text>
-            </View>
+            <Provider store={createStore(reducers)}>
+                <View>
+                    <Text>Hello World</Text>
+                </View>
+            </Provider>
         );
     }
 }
