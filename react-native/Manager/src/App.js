aef105f8d91c7ef0c5c7b05f4317a5fc16d36fbf
Ivan Magda <imagda15@gmail.com>
2017-03-23 01:50:57 +0900

    [Native] Manager - project setup

---
+++ react-native/Manager/src/App.js
@@ -0,0 +1,29 @@
+
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+
+import reducers from  './reducers';
+
+export default class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyB45Oql3bN5EIDnRzt_krWMcjQWB-SgQY4",
+            authDomain: "auth-b2fce.firebaseapp.com",
+            databaseURL: "https://auth-b2fce.firebaseio.com",
+            storageBucket: "auth-b2fce.appspot.com",
+            messagingSenderId: "716119981017"
+        });
+    }
+    render() {
+        return (
+            <Provider store={createStore(reducers)}>
+                <View style={{ flex: 1 }}>
+                    <Text>Hello World !!!</Text>
+                </View>
+            </Provider>
+        );
+    }
+}
