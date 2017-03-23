6c069d9de8c630fcbad0991789d95a6522287610
Ivan Magda <imagda15@gmail.com>
2017-03-23 13:09:26 +0900

    [Native] Manager - Navigate to the EmployeeCreate form

--- react-native/Manager/src/Router.js
+++ react-native/Manager/src/Router.js
@@ -1,9 +1,10 @@
 
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 export default RouterComponent = () => {
     return (
@@ -13,7 +14,19 @@ export default RouterComponent = () => {
             </Scene>
 
             <Scene key="main">
-                <Scene key="employeeList" component={EmployeeList} title="Employees" />
+                <Scene
+                    key="employeeList"
+                    component={EmployeeList}
+                    title="Employees"
+                    rightTitle="Add"
+                    onRight={() => Actions.employeeCreate() }
+                    initial
+                />
+                <Scene
+                    key="employeeCreate"
+                    component={EmployeeCreate}
+                    title="Create Employee"
+                />
             </Scene>
         </Router>
     );
---
+++ react-native/Manager/src/components/EmployeeCreate.js
@@ -0,0 +1,13 @@
+
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class EmployeeCreate extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Employee Form</Text>
+            </View>
+        );
+    }
+}
